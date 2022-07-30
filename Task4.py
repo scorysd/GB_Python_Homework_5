b43a4767574dad8978dfe5b70c6447d57dc3c811
Roman <toolatet@gmail.com>
2022-07-30 13:11:45 +0700

    modify

--- Task4.py
+++ Task4.py
@@ -1,5 +1,7 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
-str1 = 'wyyyyyyee4egggg555feeuuuhhkjhkuuu'
+from itertools import repeat
+with open("input.txt", "r") as file:
+    str1 =  file.read()
 def coding(str):
     code = []
     count = 1
@@ -11,9 +13,21 @@ def coding(str):
                 code.append(count)
                 code.append(str[i])
                 count = 1
+    code.append(count)
+    code.append(str[-1])
     return code
-def write_in_file (str1):
-    with open("coding.txt", "w") as file:
-        file.write((" ".join(map(str,coding(str1)))))
-write_in_file(str1)
-print(coding(str1))
+def uncoding(str):
+    str = list(str)
+    code = []
+    i =0
+    while i in range(len(str)-1):
+        str[i] = int(str[i])
+        code.extend(repeat(str[i+1], str[i]))
+        i += 2
+    return code
+def write_in_file (str1, func):
+    with open("output.txt", "w") as file:
+        file.write(("".join(map(str,func(str1)))))
+uncoding(str1)                  # функция восстановления
+coding(str1)                    # функция сжатия
+write_in_file(str1, coding)     # неоюходимо выбрать нужную функцию
